347c7f25a5b99dff0787b2bfeeef58f04949e395
navi5599 <ivkelava10@gmail.com>
2022-07-03 22:30:44 +0200

    Improved styling,and added onlick function to add button

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
 function newItem() {
-    //adding li to the ol
-    let li = $('<li></li>')
-    let inputValue = $('#input').val();
-    li.append(inputValue);
+  const li = $('<li></li>');
+  const inputValue = $('#input').val();
+  li.append(inputValue);
 
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        let list = $('#list');
-        list.append(li);
-    };
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else {
+    const list = $('#list');
+    list.append(li);
+  };
 
-    //function that crosses li items
-    li.on("dblclick",function crossOut() {
-        li.toggleClass("strike");
-    });
+  // function that crosses li items
+  li.on('dblclick', () => {
+    li.toggleClass('strike');
+  });
 
-    //here is the delete button X 
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode("X"));
-    li.append(crossOutButton);
+  // here is the delete button X
+  const crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
 
-    crossOutButton.on("click", deleteListItem);
-    function deleteListItem() {
-        li.addClass("delete")
-    };
+  function deleteListItem() {
+    li.addClass('delete');
+  }
 
-    $('#list').sortable();
-    
-};
+  crossOutButton.on('click', deleteListItem);
+
+  $('#list').sortable();
+}
+
+// Removes input value after add button is clicked
+const btn = document.getElementById('button');
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const inputValue = document.getElementById('input');
+  inputValue.value = '';
+});
